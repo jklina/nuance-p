9b95c0317a93898619117587a42e734b3800ab64
jklina <jklina@localhost>
2012-01-24 00:14:41 -0500

    adding about page.

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'About' })
+};
+
+
+exports.about = function(req, res){
+  res.render('about', { title: 'About' })
 };
